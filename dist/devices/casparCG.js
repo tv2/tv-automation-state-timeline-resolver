2526ccb200889de4d4547dc4ec0090d970b0f312
Tom Lee <thomasslee97@gmail.com>
2020-02-21 11:17:04 +0000

    fix: Caspar retry timer

--- dist/devices/casparCG.js
+++ dist/devices/casparCG.js
@@ -11,7 +11,6 @@ const request = require("request");
 const MAX_TIMESYNC_TRIES = 5;
 const MAX_TIMESYNC_DURATION = 40;
 const MEDIA_RETRY_INTERVAL = 10 * 1000; // default time in ms between checking whether a file needs to be retried loading
-const MEDIA_RETRY_DEBOUNCE = 500; // how long to wait after a command has sent before checking for retries
 /**
  * This class is used to interface with CasparCG installations. It creates
  * device states from timeline states and then diffs these states to generate
@@ -598,7 +597,7 @@ class CasparCGDevice extends device_1.DeviceWithState {
         if (!context.match(/\[RETRY\]/i)) {
             clearTimeout(this._retryTimeout);
             if (!this.initOptions || this.initOptions.retryInterval !== false)
-                this._retryTimeout = setTimeout(() => this._assertIntendedState(), MEDIA_RETRY_DEBOUNCE);
+                this._retryTimeout = setTimeout(() => this._assertIntendedState(), this._retryTime);
         }
         let cwc = {
             context: context,
@@ -611,7 +610,31 @@ class CasparCGDevice extends device_1.DeviceWithState {
             if (this._queue[resCommand.token]) {
                 delete this._queue[resCommand.token];
             }
-            this._ccgState.applyCommands([{ cmd: resCommand.serialize() }], time);
+            // If the command was performed successfully, copy the state from the current state into the tracked caspar-state:
+            // This is later used in _assertIntendedState
+            if ((resCommand.name === 'LoadbgCommand' ||
+                resCommand.name === 'PlayCommand' ||
+                resCommand.name === 'LoadCommand') &&
+                resCommand.channel &&
+                resCommand.layer) {
+                const currentState = this.getState(time);
+                if (currentState) {
+                    const currentCasparState = this.convertStateToCaspar(currentState.state);
+                    const trackedState = this._ccgState.getState();
+                    const channel = currentCasparState.channels[resCommand.channel];
+                    if (!trackedState.channels[resCommand.channel]) {
+                        trackedState.channels[resCommand.channel] = {
+                            channelNo: channel.channelNo,
+                            fps: channel.fps || 0,
+                            videoMode: channel.videoMode || null,
+                            layers: {}
+                        };
+                    }
+                    // Copy the tracked from current state:
+                    trackedState.channels[resCommand.channel].layers[resCommand.layer] = currentCasparState.channels[resCommand.channel].layers[resCommand.layer];
+                    this._ccgState.setState(trackedState);
+                }
+            }
         }).catch((error) => {
             let errorString = '';
             if (error && error.response && error.response.code === 404) {
